b6ee3ac6c8dbbeea3c76930096c54522c71936b8
danielreinhard1129 <danielreinhard1129@gmail.com>
2026-06-29 09:30:29 +0700

    initial commit

---
+++ index.ts
@@ -0,0 +1,23 @@
+import express from "express";
+import { userRoutes } from "./routes/user.routes.js";
+import { globalError, notFoundError } from "./utils/errors.js";
+
+const PORT = 8000;
+
+const app = express();
+
+// configs
+app.use(express.json()); // agar bisa menerima req.body
+
+// entry point
+app.use("/users", userRoutes);
+
+// errors
+app.use(globalError);
+app.use(notFoundError);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port : ${PORT}`);
+});
+
+// pigiri3887@googxs.com
