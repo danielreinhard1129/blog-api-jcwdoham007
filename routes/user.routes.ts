dc75421f9b2f05ca1735237be89dac4b54780e3d
Daniel Reinhard <119338658+danielreinhard1129@users.noreply.github.com>
2026-07-09 10:17:53 +0700

    feat: add rbac (#9)

--- routes/user.routes.ts
+++ routes/user.routes.ts
@@ -7,10 +7,16 @@ import {
   updateUserController,
 } from "../controllers/user.controller.js";
 import { createUserValidator } from "../validators/user.validator.js";
+import { verifyRole, verifyToken } from "../middlewares/auth.middleware.js";
 
 const userRoutes = express.Router();
 
-userRoutes.get("/", getUsersController);
+userRoutes.get(
+  "/",
+  verifyToken(process.env.JWT_SECRET!),
+  verifyRole(["SUPER_ADMIN", "ADMIN"]),
+  getUsersController,
+);
 userRoutes.get("/:id", getUserController);
 userRoutes.post("/", createUserValidator, createUserController);
 userRoutes.patch("/:id", updateUserController);
