06a0a2f8e0efc74b68d008bf30b26d6231173fdc
danielreinhard1129 <danielreinhard1129@gmail.com>
2026-07-16 10:39:45 +0700

    fix: add dotenv config

--- config/cors.ts
+++ config/cors.ts
@@ -5,6 +5,7 @@ export const corsOptions: CorsOptions = {
   origin: [
     "http://localhost:5173",
     "http://localhost:4173",
+    "https://blog-api-jcwdoham007.vercel.app",
     process.env.BASE_URL_FE!,
   ],
   credentials: true,
